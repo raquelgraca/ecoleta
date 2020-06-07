f21a6e0d9d2ca2b0e3c8158327afb6225991f5f0
raquelgraca <raquelngraca@gmail.com>
2020-06-06 22:24:24 -0300

    Added node and mvc template

--- src/server.js
+++ src/server.js
@@ -1,1 +1,29 @@
-server.js
+const express = require("express")
+const server = express()
+
+
+//configurar pasta pública
+server.use(express.static("public"))
+
+//utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+  express: server,
+  noCache: true
+})
+
+//configurar caminhos da aplicação(routes)
+server.get("/", (req, res) => {
+  return res.render("index.html")
+})
+
+server.get("/search", (req, res) => {
+  return res.render("search-results.html")
+})
+
+server.get("/create-point", (req, res) => {
+  return res.render("create-point.html")
+})
+
+// ligar o servidor
+server.listen(3000)
